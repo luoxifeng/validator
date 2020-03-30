63c50bec228e56e1cd50b13c44a89594a8fd4c69
luoxifeng <942394682@qq.com>
2020-03-30 18:10:31 +0800

    add： add test

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # validator
 validate schema
+
+## Useage
+
+```javascript
+```
---
+++ __test__/index.js
@@ -0,0 +1,51 @@
+
+const cfg = [
+  {
+
+    id: 'showId',
+    required: {
+      value: true,
+      tip: '必填参数',
+      errorTip: '请输入showName'
+    },
+    type: {
+      value: Number,
+      tip: '必填参数',
+      errorTip: 'showId必须是Number'
+    },
+    diy: {
+      value: (value) => /^\d{6,8}$/.test(value),
+      tip: 'showId是6-8位的数字',
+      errorTip: 'showId是6-8位的数字'
+    },
+  },
+  {
+    id: 'showName',
+    type: {
+      value: String,
+      tip: '必填参数',
+      errorTip: '请输入showName'
+    }
+  },
+  {
+    id: 'showBtn',
+    required: {
+      value: true,
+      tip: '必填参数',
+      errorTip: '请输入showName'
+    },
+    type: {
+      value: Boolean,
+      tip: '必填参数',
+      errorTip: '请输入showName'
+    }
+  },
+  {
+    id: 'other',
+    type: {
+      value: String,
+      tip: 'other',
+      errorTip: 'other'
+    }
+  },
+];
